f566fc636f3e44b8bf94ca1b378633e618183bc7
austin <posty65841@gmail.com>
2022-08-31 20:48:52 -0500

    lets complete

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 // Assignment Code
 let generateBtn = document.querySelector("#generate");
-let lowerCaseAlphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-let upperCaseAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+let lowerCaseAlphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'] // lowercase options
+let upperCaseAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'] // uppercase options
+let specialCharacter = [ '!', '@', '#', '$', '%', '&', '*'] //special character options
+let numericNum =[ '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'] //number options 
+console.log ("abcdefg")
 // Write password to the #password input
 function writePassword() {
   let password = generatePassword();
